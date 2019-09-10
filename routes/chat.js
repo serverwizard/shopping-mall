f25d5daf7c4df8833e159bfc9690a05ebed619dd
hongjongwan <serverwizard89@woowahan.com>
2019-09-11 00:00:07 +0900

    [Day07] 채팅 화면용 라우터

--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.render('chat/index.html');
+router.get('/', (req, res) => {
+    if (!req.isAuthenticated()) {
+        res.send('<script>alert("로그인이 필요한 서비스입니다.");\
+        location.href="/accounts/login";</script>');
+    } else {
+        res.render('chat/index.html');
+    }
 });
 
 module.exports = router;
