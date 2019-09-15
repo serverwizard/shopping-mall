c9409dbf90ef817581de5195fca939e99f5f6d14
hongjongwan <serverwizard89@woowahan.com>
2019-09-16 00:35:56 +0900

    [Day07] server.js 파일 생성 (서버 관련 내용)

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require('./app.js');
+const port = 3000;
+
+const server = app.listen( port, function(){
+    console.log('Express listening on port', port);
+});
+
+const listen = require('socket.io');
+const io = listen(server);
+
+// socket io passport 접근하기 위한 미들웨어 적용
+// socket 에서 session 에 접근 가능하도록 설정
+io.use( (socket, next) => {
+    app.sessionMiddleWare(socket.request, socket.request.res, next);
+});
+
+require('./helpers/socketConnection')(io);
