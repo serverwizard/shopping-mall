cb6d0e87c0dae4ca4261277cc0a65a775c01b5e8
hongjongwan <serverwizard89@woowahan.com>
2019-09-16 01:50:25 +0900

    [Day07] routes -> controllers 디렉토리로 이동

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const router = Router();
+
+router.use('/', require('./home'));
+router.use('/admin', require('./admin'));
+router.use('/accounts', require('./accounts'));
+router.use('/auth', require('./auth'));
+router.use('/chat', require('./chat'));
+router.use('/products',  require('./products'));
+
+module.exports = router;
