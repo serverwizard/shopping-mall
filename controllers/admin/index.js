5655160a2bdee883dc1b82b9ef7daaa5e8517ad5
hongjongwan <serverwizard89@woowahan.com>
2019-09-16 02:08:42 +0900

    [Day07] admin 컨트롤러 내용 추가, products 컨트롤러 제거

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,14 +1,23 @@
 const { Router } = require('express');
 const router = Router();
 const ctrl = require('./admin.ctrl');
-const paginate = require('express-paginate');
 const csrfProtection = require('../../middleware/csrf');
 
 const loginRequired = require('../../middleware/loginRequired');
 const upload = require('../../middleware/multer');
 
+const paginate = require('express-paginate');
+
 router.get('/products', paginate.middleware(3, 50), ctrl.get_products );
+
 router.get('/products/write', loginRequired, csrfProtection , ctrl.get_write );
 router.post('/products/write' , loginRequired , upload.single('thumbnail'),  csrfProtection, ctrl.post_write);
+router.get('/products/detail/:id', ctrl.get_product_detail);
+router.post('/products/detail/:id', ctrl.post_product_detail);
+router.get('/products/edit/:id', loginRequired, csrfProtection, ctrl.get_edit);
+router.post('/products/edit/:id', loginRequired, upload.single('thumbnail'), csrfProtection, ctrl.post_edit);
+router.get('/products/delete/:id', ctrl.delete_product);
+router.get('/products/delete/:product_id/:memo_id', ctrl.delete_memo_of_product);
+router.post('/products/ajax_summernote', loginRequired, upload.single('thumbnail'), ctrl.post_summernote);
 
 module.exports = router;
