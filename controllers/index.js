46314609fca8f227051b8a61ccaa09ad0fdc23a6
hongjongwan <serverwizard89@woowahan.com>
2019-09-16 02:09:06 +0900

    [Day07] products 컨트롤러 제거

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,5 @@ router.use('/admin', require('./admin'));
 router.use('/accounts', require('./accounts'));
 router.use('/auth', require('./auth'));
 router.use('/chat', require('./chat'));
-router.use('/products',  require('./products'));
 
 module.exports = router;
