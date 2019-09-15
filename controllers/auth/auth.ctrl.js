5c08d78894ff0bed790b231ade28783c56848c54
hongjongwan <serverwizard89@woowahan.com>
2019-09-16 01:38:24 +0900

    [Day07] 폴더별로 컨트롤러 분리 (facebook_auth)

---
+++ controllers/auth/auth.ctrl.js
@@ -0,0 +1,9 @@
+//로그인 성공시 이동할 주소
+exports.redirect_facebook_success = (req, res) => {
+    // res.send(req.user);
+    res.redirect('/admin/products')
+};
+
+exports.redirect_facebook_fail = (req, res) => {
+    res.send('facebook login fail');
+};
