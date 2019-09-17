b5c3406b664e527192e171bea1d198bb3aa91468
hongjongwan <serverwizard89@woowahan.com>
2019-09-18 01:18:34 +0900

    [Day09] common.js 구현 (쿠키 관련 기능)

--- static/common.js
+++ static/common.js
@@ -1,9 +1,16 @@
-function getCookie(cname){
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(unescape(document.cookie));
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
+const getCookie = cname => {
+    let name = cname + "=";
+    // 암호화된 URI 해독
+    console.log('document.cookie : ' + document.cookie);
+    let decodedCookie = decodeURIComponent(unescape(document.cookie));
+    console.log('decodedCookie : ' + decodedCookie);
+    // cartList={"1":{"number":"1","amount":"1000000000","thumbnail":"products-1568121079438.jpeg","name":"맥북"}}
+    let ca = decodedCookie.split(';');
+    console.log('decodedCookie.split(";") : ' + ca);
+    // cartList={"1":{"number":"1","amount":"1000000000","thumbnail":"products-1568121079438.jpeg","name":"맥북"}}
+
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
@@ -12,12 +19,12 @@ function getCookie(cname){
         }
     }
     return "";
-}
+};
 
-function setCookieHour( name, value, hours ){
-    var now = new Date();
-    var time = now.getTime();
+const setCookieHour = (name, value, hours) => {
+    let now = new Date();
+    let time = now.getTime();
     time += 3600 * 1000 * hours;
     now.setTime(time);
-    document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + now.toUTCString() + ";"
-}
+    document.cookie = name + "=" + escape(value) + "; path=/; expires=" + now.toUTCString() + ";"
+};
