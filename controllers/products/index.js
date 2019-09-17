dfba6202184e7d8cbae01c3e911474e0a83a36a2
hongjongwan <serverwizard89@woowahan.com>
2019-09-18 01:16:24 +0900

    [Day09] 상품 상세 화면 구현

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const router = express.Router();
 const models = require('../../models');
 
-router.get('/:id' , async(req, res) => {
-    try{
+router.get('/:id', async (req, res) => {
+    try {
         const product = await models.Products.findByPk(req.params.id);
-        res.render('products/detail.html', { product });
-
-    }catch(e){
+        res.render('products/detail.html', {product});
+    } catch (e) {
         console.error(e);
     }
 });
-
 
 module.exports = router;
