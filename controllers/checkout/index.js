ab08e6e3340ccea5df39d89e7166d593cd14ddab
hongjongwan <serverwizard89@woowahan.com>
2019-09-23 04:02:33 +0900

    [Day10] 결제 컨트롤러 생성

--- controllers/checkout/index.js
+++ controllers/checkout/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const ctrl = require('./checkout.ctrl');
 
 router.get('/', ctrl.index);
+router.get('/complete', ctrl.get_complete);
+router.post('/complete', ctrl.post_complete);
+router.get('/success', ctrl.get_success);
 
 module.exports = router;
