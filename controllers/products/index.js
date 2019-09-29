e2803ae5ea16d8e6c53210b661209e65095a0e4d
hongjongwan <serverwizard89@woowahan.com>
2019-09-30 08:58:52 +0900

    [Day12] 좋아요 관련 컨트롤러 추가

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -2,12 +2,32 @@ const express = require('express');
 const router = express.Router();
 const models = require('../../models');
 
+const loginRequired = require('../../middleware/loginRequired');
+
 router.get('/:id', async (req, res) => {
     try {
         const product = await models.Products.findByPk(req.params.id);
-        res.render('products/detail.html', {product});
+
+        // 좋아요 내용을 가져온다
+        const userLikes = await require('../../helpers/userLikes')(req);
+
+        res.render('products/detail.html', {product, userLikes});
     } catch (e) {
         console.error(e);
+    }
+});
+
+router.post('/like/:product_id(\\d+)', loginRequired, async (req, res) => {
+    try {
+        const product = await models.Products.findByPk(req.params.product_id);
+        const user = await models.User.findByPk(req.user.id);
+
+        const status = await user.addLikes(product);
+
+        res.json({status});
+
+    } catch (e) {
+        console.log(e);
     }
 });
 
