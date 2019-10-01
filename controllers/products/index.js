0998975960a43d180ab9d0cd412ee71a1f6d1372
hongjongwan <serverwizard89@woowahan.com>
2019-10-01 19:11:19 +0900

    [Day13] 좋아요 해제 기능 구현

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -31,4 +31,19 @@ router.post('/like/:product_id(\\d+)', loginRequired, async (req, res) => {
     }
 });
 
+router.delete('/like/:product_id(\\d+)', loginRequired, async (req, res) => {
+    try {
+        const product = await models.Products.findByPk(req.params.product_id);
+        const user = await models.User.findByPk(req.user.id);
+
+        await user.removeLikes(product);
+
+        res.json({
+            message: "success"
+        });
+    } catch (e) {
+
+    }
+});
+
 module.exports = router;
--- static/common.js
+++ static/common.js
@@ -29,7 +29,7 @@ const setCookieHour = (name, value, hours) => {
     document.cookie = name + "=" + escape(value) + "; path=/; expires=" + now.toUTCString() + ";"
 };
 
-$(document).on( 'click' , '.add_like' , function(e){
+$(document).on('click', '.add_like', function (e) {
     // 주소에 # 붙는것 방지
     e.preventDefault();
 
@@ -40,21 +40,47 @@ $(document).on( 'click' , '.add_like' , function(e){
     var product_id = $(this).attr('product_id');
 
     $.ajax({
-        url : '/products/like/'+ product_id,
-        type : 'post',
+        url: '/products/like/' + product_id,
+        type: 'post',
     })
-        .done(function(){
+        .done(function () {
 
             $('#' + target_id).html('\
-            <a product_id="'+ product_id + '" class="pull-right remove_like" href="#"> \
+            <a product_id="' + product_id + '" class="pull-right remove_like" href="#"> \
                 <img src="/static/img/likeon.png" width="20" alt=""> \
             </a> \
         ');
 
         })
-        .fail(function(){
+        .fail(function () {
             console.log('오류발생');
         })
+});
 
+$(document).on('click', '.remove_like', function (e) {
+    // 주소에 # 붙는것 방지
+    e.preventDefault();
 
+    // 부모 요소의 아이디를 가져온다.
+    var target_id = $(this).parent().attr('id');
+
+    // 현재 제품의 id 받아옴
+    var product_id = $(this).attr('product_id');
+
+    $.ajax({
+        url: '/products/like/' + product_id,
+        type: 'delete',
+    })
+        .done(function () {
+
+            $('#' + target_id).html('\
+            <a product_id="' + product_id + '" class="pull-right add_like" href="#"> \
+                <img src="/static/img/likeoff.png" width="20" alt=""> \
+            </a> \
+        ');
+
+        })
+        .fail(function () {
+            console.log('오류발생');
+        })
 });
